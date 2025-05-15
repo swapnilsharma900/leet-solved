a30eba23c52fe8323b576656fc84121a97415d55
Swapnil Sharma <148647151+swapnilsharma900@users.noreply.github.com>
2025-05-15 21:03:40 +0000

    Sync LeetCode submission Runtime - 5 ms (82.17%), Memory - 43.8 MB (99.99%)

--- solutions/0009-palindrome-number/solution.java
+++ solutions/0009-palindrome-number/solution.java
@@ -1,12 +1,21 @@
+import java.util.*;
 class Solution {
     public boolean isPalindrome(int x) {
-        if (x<0 || (x > 1 && x <= 100 && x%10 == 0))
-            return false;
-        StringBuffer sb = new StringBuffer(Integer.toString(x));
-        if (sb.toString().equals(sb.reverse().toString()))
-            return true;
-        else
-            return false;
+        // StringBuilder sb = new StringBuilder(Integer.toString(x));
+        // if(sb == sb.reverse())
+        //     return true;
+        // return false;
+
+        int temp = x, last, rev = 0;
+        while(temp>0){
+            last = temp%10;
+            rev = rev*10+last;
+            temp = temp/10;
+        }
+        if(x == rev) return true;
+        else return false;
+
+
+
     }
 }
-
