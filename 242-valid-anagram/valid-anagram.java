57d4f7f1702986d02f078b8f418c43b48921a82a
Swapnil Sharma <148647151+swapnilsharma900@users.noreply.github.com>
2026-09-23 00:33:28 +0530

    Time: 4 ms (83.76%) | Memory: 44.3 MB (73.99%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -8,12 +8,30 @@
 import java.util.Arrays;
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length() != t.length()) return false;
-        char[] sa = s.toCharArray();
-        char[] ta = t.toCharArray();
-        Arrays.sort(sa);
-        Arrays.sort(ta);
-        if(Arrays.equals(sa, ta))
+        // if(s.length() != t.length()) return false;
+        // char[] sa = s.toCharArray();
+        // char[] ta = t.toCharArray();
+        // Arrays.sort(sa);
+        // Arrays.sort(ta);
+        // if(Arrays.equals(sa, ta))
+        //     return true;
+        // return false;
+
+
+        // --- APPROACH TWO ---
+        int[] freqOfS = new int[26];
+        int[] freqOfT = new int[26];
+
+        for(int i=0; i<s.length(); i++){
+            char ch = s.charAt(i);
+            freqOfS[ch-97]++;
+        }
+        for(int i=0; i<t.length(); i++){
+            char ch = t.charAt(i);
+            freqOfT[ch-97]++;
+        }
+
+        if(Arrays.equals(freqOfS, freqOfT))
             return true;
         return false;
     }
