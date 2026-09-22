e12e13584a299d6d6e26270535cfc5f313e11ec4
Swapnil Sharma <148647151+swapnilsharma900@users.noreply.github.com>
2026-09-23 00:47:05 +0530

    Time: 6 ms (44.27%) | Memory: 44.4 MB (73.99%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -2,12 +2,20 @@
 # INTUITION
     sort both strings in alphbatical order then compare
 # DESCRIPTION
-    convert both strings to char Array
+    convert both strings to char Array then sort them and compare them if they are equal or not.
+# OPTIMIZATION
+    create 2 array of size 26 (for s and t), increment the frequency of character everytime it appears in perticular string.
+    Compare both string if they are equal or not
+# OPTIMIZATION 2
+    create one array of size 26 and increment the frequency of character if it appears in s and decrement its frequency if it appears in t.
+    Compare and if the array is empty return true
  */
 
 import java.util.Arrays;
 class Solution {
     public boolean isAnagram(String s, String t) {
+
+        // --- APPROACH ONE ---
         // if(s.length() != t.length()) return false;
         // char[] sa = s.toCharArray();
         // char[] ta = t.toCharArray();
@@ -19,20 +27,36 @@ class Solution {
 
 
         // --- APPROACH TWO ---
-        int[] freqOfS = new int[26];
-        int[] freqOfT = new int[26];
+        // int[] freqOfS = new int[26];
+        // int[] freqOfT = new int[26];
 
-        for(int i=0; i<s.length(); i++){
+        // for(int i=0; i<s.length(); i++){
+        //     char ch = s.charAt(i);
+        //     freqOfS[ch-97]++;
+        // }
+        // for(int i=0; i<t.length(); i++){
+        //     char ch = t.charAt(i);
+        //     freqOfT[ch-97]++;
+        // }
+
+        // if(Arrays.equals(freqOfS, freqOfT))
+        //     return true;
+        // return false;
+
+
+        // --- APPROACH THREE ---
+        if(s.length() != t.length()) return false;
+
+        int[] freq = new int[26];
+        for (int i=0; i<s.length(); i++){
             char ch = s.charAt(i);
-            freqOfS[ch-97]++;
+            freq[ch-97]++;
+            ch = t.charAt(i);
+            freq[ch-97]--;
         }
-        for(int i=0; i<t.length(); i++){
-            char ch = t.charAt(i);
-            freqOfT[ch-97]++;
-        }
+        for(int i=0; i<26; i++)
+            if(freq[i] != 0) return false;
+        return true;
 
-        if(Arrays.equals(freqOfS, freqOfT))
-            return true;
-        return false;
     }
 }
