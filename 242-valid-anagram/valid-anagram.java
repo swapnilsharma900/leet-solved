0652e2ab3d00ba1253dd9399e295aeb16925c06e
Swapnil Sharma <148647151+swapnilsharma900@users.noreply.github.com>
2026-09-23 00:08:03 +0530

    Time: 6 ms (44.27%) | Memory: 46.6 MB (24.52%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.java
@@ -0,0 +1,19 @@
+/**
+# INTUITION
+    sort both strings in alphbatical order then compare
+# DESCRIPTION
+    convert both strings to char Array
+ */
+
+import java.util.Arrays;
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        char[] sa = s.toCharArray();
+        char[] ta = t.toCharArray();
+        Arrays.sort(sa);
+        Arrays.sort(ta);
+        if(Arrays.equals(sa, ta))
+            return true;
+        return false;
+    }
+}
