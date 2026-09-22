dc4e36786e2b0033d4ff14f32169c0a80df06cbf
Swapnil Sharma <148647151+swapnilsharma900@users.noreply.github.com>
2026-09-23 00:10:23 +0530

    Time: 4 ms (83.76%) | Memory: 46.4 MB (33.31%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -8,6 +8,7 @@
 import java.util.Arrays;
 class Solution {
     public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()) return false;
         char[] sa = s.toCharArray();
         char[] ta = t.toCharArray();
         Arrays.sort(sa);
